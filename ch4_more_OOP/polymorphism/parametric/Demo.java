94f41d1250fe7f1896914c76a2c104c6e68d2981
tariq-abdulghani <tariq.abdulghani.hussein@gmail.com>
2025-10-10 10:44:06 +0300

    adding Ch4 code, generics, wildcard and wildcard capture

--- ch4_more_OOP/polymorphism/parametric/Demo.java
+++ ch4_more_OOP/polymorphism/parametric/Demo.java
@@ -1,10 +1,53 @@
 package ch4_more_OOP.polymorphism.parametric;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
         OrderedPair<Integer, String> p1 = new OrderedPair<>(1, "apple");
         OrderedPair<Integer, String> p2 = new OrderedPair<>(2, "pear");
         boolean same = Util.compare(p1, p2);
         System.out.printf("%s equals %s is %s%n", p1, p2, same);
+
+        List<String> names = new ArrayList<>();
+//        List<Object> objectList = names; // error
+        List<?> objects = names;
+        List rawList = objects;
+
+
+//  Wildcard demos
+        // can be considered readonly since you cannot apply any modifications that requires type capture without helper methods
+        List<? extends Number> numbers;
+        List<Integer> integers = new ArrayList<>();
+        List<Double> doubles = new ArrayList<>();
+
+        numbers = integers;
+//        errors 
+//        numbers.add( new Double(9.0));
+//        numbers.add(90);
     }
+
+    static void foo(List<?> list){
+//        type capture error why? we can not set any item in the list
+//        ,but we don't know what type should be included.
+//        list.set(0, list.get(0));
+        fooHelper(list);
+    }
+
+//    static void foo(List<?> list){
+////        type capture error why? we can not set any item in the list
+////        ,but we don't know what type should be included.
+//        list.set(0, list.get(0));
+//    }
+
+//    with T we made the constraint that all elements are of type T.
+//    so even if we don't know T but all items in the list will be
+//    of the same type.
+    private static <T>void fooHelper(List<T> list){
+        list.set(0, list.get(0));
+    }
+
+
+
 }
